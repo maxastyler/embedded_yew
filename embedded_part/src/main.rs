cd88de9bfa5f97298badc896656bb4fe2a1bee19
Max Tyler <maxastyler@gmail.com>
2024-02-06 20:45:08 +0000

    Get the embedded part compiling

--- embedded_part/src/main.rs
+++ embedded_part/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
-}
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+
+use defmt as _;
+use defmt_rtt as _;
+use panic_probe as _;
+
+embassy_rp::bind_interrupts!(struct Irqs {
+});
+
+#[embassy_executor::main]
+async fn main(spawner: embassy_executor::Spawner) {}
